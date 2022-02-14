2e77eb48b36c470be8a8d42fcf4e0fb0da295226
FrtsVictor <victorfreitas.job@gmail.com>
2022-02-13 23:54:08 -0300

    Fixed Names

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { LOG_COLORS } from './reader/utils/log-colors';
 import { LogType } from './reader/models/log-name.enum';
-import { startReaderByErrorType } from './reader/reader';
 import { customLog } from './reader/utils/custom-log';
+import { ColdFusionLogReader } from './reader';
 
 (async () => {
-  await startReaderByErrorType(LogType.EXCEPTION, LOG_COLORS.text.red);
-  await startReaderByErrorType(LogType.REST_SERVICE, LOG_COLORS.text.yellow);
-  await startReaderByErrorType(LogType.SERVER, LOG_COLORS.text.green);
+  const reader = new ColdFusionLogReader();
+  await reader.startReaderByErrorType(LogType.EXCEPTION, LOG_COLORS.text.red);
+  await reader.startReaderByErrorType(
+    LogType.REST_SERVICE,
+    LOG_COLORS.text.yellow
+  );
+  await reader.startReaderByErrorType(LogType.SERVER, LOG_COLORS.text.green);
   customLog(LOG_COLORS.text.cyan, 'App Running');
 })();
---
+++ src/reader/index.ts
@@ -0,0 +1,2 @@
+export * from './cold-fusion-log-reader';
+export * from './cold-fusion-log-reader.interface';
