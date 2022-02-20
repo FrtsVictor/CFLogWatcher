3efaa5ebb0cbd09afd10c2ffbfece9d947c1bb1b
FrtsVictor <victorfreitas.job@gmail.com>
2022-02-20 13:51:38 -0300

    Added logFactory

---
+++ src/reader/factory/cold-fusion-log-factory.ts
@@ -0,0 +1,18 @@
+import { ColdFusionExceptionLog } from '../models/cold-fusion-exception-log.model';
+import { ColdFusionInformationLog } from '../models/cold-fusion-information-log';
+import { ColdFusionLogBase } from '../models/cold-fusion-log-base.model';
+import { LogType } from '../models/log-type.enum';
+
+export class ColdFusionLogFactory {
+  public static create(logType: LogType, message: string): ColdFusionLogBase {
+    switch (logType) {
+      case LogType.EXCEPTION:
+        return new ColdFusionExceptionLog(message);
+      case LogType.REST_SERVICE:
+      case LogType.SERVER:
+        return new ColdFusionInformationLog(logType, message);
+      default:
+        throw new Error('Check log type');
+    }
+  }
+}
