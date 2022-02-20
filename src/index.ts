f814d5840510f66e086b5d5ca72a609e3e41d8ea
FrtsVictor <victorfreitas.job@gmail.com>
2022-02-20 13:52:09 -0300

    Implemented classes / interface

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import { LOG_COLORS } from './reader/utils/log-colors';
-import { LogType } from './reader/models/log-name.enum';
+import { LogType } from './reader/models/log-type.enum';
 import { customLog } from './reader/utils/custom-log';
 import { ColdFusionLogReader } from './reader';
 
 (async () => {
-  const reader = new ColdFusionLogReader();
-  await reader.startReaderByErrorType(LogType.EXCEPTION, LOG_COLORS.text.red);
-  await reader.startReaderByErrorType(
+  await new ColdFusionLogReader().startReaderByErrorType(
+    LogType.EXCEPTION,
+    LOG_COLORS.text.red
+  );
+
+  await new ColdFusionLogReader().startReaderByErrorType(
     LogType.REST_SERVICE,
     LOG_COLORS.text.yellow
   );
-  await reader.startReaderByErrorType(LogType.SERVER, LOG_COLORS.text.green);
+
+  await new ColdFusionLogReader().startReaderByErrorType(
+    LogType.SERVER,
+    LOG_COLORS.text.green
+  );
   customLog(LOG_COLORS.text.cyan, 'App Running');
 })();
